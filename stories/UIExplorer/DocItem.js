5c01eb588cf31fe151f043018a624cd988fca7ad
Hoang Nam <particle4dev@gmail.com>
2018-01-01 09:47:30 +0000

    153976110-update-storybook-style

---
+++ stories/UIExplorer/DocItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import reactElementToJSXString from 'react-element-to-jsx-string';
+// import PropTypes from 'prop-types'
+// import ClassNames from 'classnames'
+import Code from './Code'
+
+class DocItem extends React.PureComponent {
+
+  render () {
+    const { render } = this.props
+    return (
+      <div style={{marginRight: '20px'}}>
+        {render}
+        <Code>
+          {reactElementToJSXString(render, {
+            showDefaultProps: false,
+          }).toString()}
+        </Code>
+      </div>
+    )
+  }
+}
+
+DocItem.propTypes = {}
+
+DocItem.defaultProps = {}
+
+export default DocItem
--- stories/loading.js
+++ stories/loading.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
-
 import {
   Loading,
   Spinner,
 } from '../src/index'
+import {
+  DocItem,
+} from './UIExplorer'
 
 storiesOf('Loading', module)
-  .add('progress bar', () => <Loading />)
-  .add('default spinner', () => <Spinner />)
-  .add('single color spinner', () => <Spinner singleColor />)
+  .add('progress bar', () => <DocItem
+    render={<Loading />}
+  />)
+  .add('default spinner', () => <DocItem
+  render={<Spinner />}
+  />)
+  .add('single color spinner', () => <DocItem
+  render={<Spinner singleColor />}
+  />)
